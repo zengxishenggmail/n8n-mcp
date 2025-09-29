5dc5205b2789f5a7ed2bdcedc162e164dfc39fee
github-action-benchmark <github@users.noreply.github.com>
2025-09-29 03:49:37 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for 5825a85ccc504e36e16bad3437546bfdc1dd4fb5

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758523918126,
+  "lastUpdate": 1759117777240,
   "repoUrl": "https://github.com/zengxishenggmail/n8n-mcp",
   "entries": {
     "n8n-mcp Benchmarks": [
@@ -52,6 +52,58 @@ window.BENCHMARK_DATA = {
             "range": "0.3535",
             "unit": "ms",
             "extra": "14796 ops/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "56956555+czlonkowski@users.noreply.github.com",
+            "name": "Romuald Członkowski",
+            "username": "czlonkowski"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5825a85ccc504e36e16bad3437546bfdc1dd4fb5",
+          "message": "Merge pull request #234 from czlonkowski/feat/telemetry-system-clean\n\nfeat: telemetry system refactor with enhanced privacy and reliability (v2.14.1)",
+          "timestamp": "2025-09-26T19:36:19+02:00",
+          "tree_id": "c76f224c253ab6de13fcc92d1774bf09518fe522",
+          "url": "https://github.com/zengxishenggmail/n8n-mcp/commit/5825a85ccc504e36e16bad3437546bfdc1dd4fb5"
+        },
+        "date": 1759117776570,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0192,
+            "range": "0.2231",
+            "unit": "ms",
+            "extra": "52166 ops/sec"
+          },
+          {
+            "name": "sample - array sorting - large",
+            "value": 3.23,
+            "range": "2.8466",
+            "unit": "ms",
+            "extra": "310 ops/sec"
+          },
+          {
+            "name": "sample - string concatenation",
+            "value": 0.0049,
+            "range": "0.2789",
+            "unit": "ms",
+            "extra": "205752 ops/sec"
+          },
+          {
+            "name": "sample - object creation",
+            "value": 0.0658,
+            "range": "0.30720000000000003",
+            "unit": "ms",
+            "extra": "15206 ops/sec"
           }
         ]
       }
